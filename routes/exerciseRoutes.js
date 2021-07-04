6069d55eb90578257a4ade9d4877f4f5048a4b47
Jacob Zavita <jacobzavita@gmail.com>
2021-07-04 09:17:12 -0700

    clean up exerciseRoutes

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -2,38 +2,28 @@ const router = require('express').Router()
 const { Exercise, User } = require('../models')
 
 // GET all exercises
-router.get('/exercises', (req, res) => {
-  Exercise.find()
+router.get('/exercises', (req, res) => Exercise.find()
     .then(exercises => res.json(exercises))
-    .catch(err => console.log(err))
-})
+    .catch(err => console.log(err)))
 
 // GET one exercise
-router.get('/exercises/:id', (req, res) => {
-  Exercise.findById(req.params.id)
+router.get('/exercises/:id', (req, res) => Exercise.findById(req.params.id)
     .then(exercise => res.json(exercise))
-    .catch(err => console.log(err))
-})
+    .catch(err => console.log(err)))
 
 // POST one exercise
-router.post('/exercises', (req, res) => {
-  Exercise.create(req.body)
+router.post('/exercises', (req, res) => Exercise.create(req.body)
     .then(exercise => res.json(exercise))
-    .catch(err => console.log(err))
-})
+    .catch(err => console.log(err)))
 
 // PUT one exercise
-router.put('/exercises/:id', (req, res) => {
-  Exercise.findByIdAndUpdate(req.params.id, { $set: req.body })
+router.put('/exercises/:id', (req, res) => Exercise.findByIdAndUpdate(req.params.id, { $set: req.body })
     .then(() => res.sendStatus(200))
-    .catch(err => console.log(err))
-})
+    .catch(err => console.log(err)))
 
 // DELETE one exercise
-  router.delete('/exercises/:id', (req, res) => {
-    Exercise.findByIdAndDelete(req.params.id)
+  router.delete('/exercises/:id', (req, res) => Exercise.findByIdAndDelete(req.params.id)
       .then(() => res.sendStatus(200))
-      .catch(err => console.log(err))
-})
+      .catch(err => console.log(err)))
 
 module.exports = router
---
+++ routes/userRoutes.js
@@ -0,0 +1,3 @@
+const router = require('express').Router()
+const { User, Exercise } = require('../models')
+
